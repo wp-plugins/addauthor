6bc205e08ed98f976df42ceddc4f1dbc5dac9585
wolly <wolly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-17 22:21:49 +0000

    version 1.2 changed CSS
    git-svn-id: https://plugins.svn.wordpress.org/addauthor/trunk@184108 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addauthor.php
+++ addauthor.php
@@ -2,15 +2,15 @@
 /**
  * @package addauthor
  * @author Paolo Valenti
- * @version 1.1 changed deprecated functions
+ * @version 1.2 changed CSS to have minimum height
  */
 /*
 Plugin Name: addauthor
-Plugin URI: http://www.paolo.valenti.name/2009/02/04/add-author-plugin
+Plugin URI: http://wol.ly/2009/02/04/add-author-plugin
 Description: This plugin allow you to show the description of the author of the post. Useful for multi authors blog. 
 Author: Paolo Valenti
-Version: 1.1
-Author URI: http://www.paolo.valenti.name
+Version: 1.2
+Author URI: http://wol.ly
 */
 /*  
 	Copyright 2009  Paolo Valenti aka Wolly  (email : wolly66@gmail.com)
@@ -30,9 +30,9 @@ Author URI: http://www.paolo.valenti.name
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-
 function wolly_addaut() {
-echo '<div class="addaut">';
+echo '<ul class="addaut">';
+echo '<li>';
 echo get_avatar( get_the_author_meta('user_email'), $size = '96' );
 echo 'Post by ' ;
 the_author() ;
@@ -40,6 +40,8 @@ echo ' on ';
 the_time('F jS, Y');
 echo '<br />';
 the_author_meta(description) ;
-echo '</div>';
+echo '<div class="clear"></div>';
+echo '</li>';
+echo '</ul>';
 } 
 ?>
